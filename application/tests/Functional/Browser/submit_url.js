447aed1ca3d0d6072fd981cba3a0c44121981e76
Marius Grigaitis <marius.grigaitis@nfq.lt>
2014-09-23 19:21:49 +0300

    functional test

---
+++ application/tests/Functional/Browser/submit_url.js
@@ -0,0 +1,19 @@
+require('../parameters');
+
+casper.test.begin('Url submitting', 2, function suite(test) {
+    var slug = Math.floor((Math.random() * 1000000) + 1);
+    var url = 'http://www.random.org/integers/?num=100&min=1&max=' + slug + '&col=5&base=10&format=html&rnd=new';
+
+    casper.start(document.app.parameters.base_url + '/crawler/crawl', function () {
+        test.assertExists('form', 'Form must exist');
+        this.fill('form', {url: url}, true);
+    });
+
+    casper.thenOpen(document.app.parameters.base_url + '/crawler/index', function () {
+        test.assertExists('.page-list a[href="' + url + '"]', 'Link must exist');
+    });
+
+    casper.run(function () {
+        test.done();
+    });
+});
---
+++ application/tests/Functional/parameters.example.js
@@ -0,0 +1,4 @@
+document.app = document.app || {};
+document.app.parameters = {
+    'base_url': 'http://localhost:8000'
+};
