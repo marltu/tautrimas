aca26731b1ee17f94d965ce574c3d441f0d8ef37
Marius Grigaitis <marius.grigaitis@nfq.lt>
2014-09-23 18:57:33 +0300

    Added testing database example

---
+++ application/config/testing/database.example.php
@@ -0,0 +1,30 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array
+(
+	'default' => array
+	(
+		'type'       => 'PDO',
+		'connection' => array(
+			/**
+			 * The following options are available for MySQL:
+			 *
+			 * string   hostname     server hostname, or socket
+			 * string   database     database name
+			 * string   username     database username
+			 * string   password     database password
+			 * boolean  persistent   use persistent connections?
+			 * array    variables    system variables as "key => value" pairs
+			 *
+			 * Ports and sockets may be appended to the hostname.
+			 */
+            'dsn' => 'mysql:host=localhost;dbname=workshop_testing',
+			'username'   => 'root',
+			'password'   => 'root',
+			'persistent' => FALSE,
+		),
+		'table_prefix' => '',
+		'charset'      => 'utf8',
+		'caching'      => FALSE,
+	),
+);
